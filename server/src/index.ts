ba8099b65939cf0a482c32291a3d9b22b14721c3
Soumya Ranjan Padhy <soumyaranjan@rayvector.com>
2025-02-02 06:10:23 +0530

    Fixed the port problem

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,7 +6,7 @@ import { db } from "./database/db";
 dotenv.config({ path: path.join(__dirname, "..", ".env") });
 
 const main = () => {
-	const port = Number(process.env.PORT) ?? 8000;
+	const port = Number(process.env.PORT) || 8000;
 	app.listen(port, "0.0.0.0", () => {
 		console.log(`Server started on port: ${port}`);
 	});
